3faace728d9438b22e5e39a5feba50c4da03abc9
Nayan-Bhisara <bhisaranayan3196@gmail.com>
2023-12-13 18:05:13 +0530

    c_9-1

---
+++ 9_1_q3.c
@@ -0,0 +1,28 @@
+/*
+Q.3 Write a Program to convert the given string in toggle case without using any string function.
+For example,
+Input:
+Enter any string: HelLo worLD
+
+Output:
+Toggle case string: hELlO WORld
+*/
+
+#include<stdio.h>
+void main(){
+	int i,j;
+	char a[11]={'H','e','l','L','o','w','o','r','L','D'};
+	
+	for(i=0;i<10;i++){
+		if(a[i]<97){
+			a[i]=a[i]+32;
+		printf("%c",a[i]);
+		
+		}else{
+			a[i]=a[i]-32;
+		printf("%c",a[i]);
+		}
+	 
+	}
+	
+}
